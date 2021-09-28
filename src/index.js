60b9d55f37edefa5666c1ed4c80ff56a1b3d7b75
Rohail Rafique <rohailrafique@gmail.com>
2021-09-28 17:34:08 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let taskItem = document.querySelector("#new-task-description");
+    console.log(e);
+    buildList(taskItem.value);
+    form.reset();
+  });
 });
+
+function buildList(value) {
+  // console.log(value)
+  let p = document.createElement("p");
+  let btn = document.createElement("button");
+
+  btn.textContent = "x";
+  p.textContent = value;
+  p.appendChild(btn);
+  document.querySelector("#list").appendChild(p);
+  btn.addEventListener("click", () => {
+    document.querySelector("#list").removeChild(p)
+  });
+}
+
+// function deleteTask() {
+//   document.querySelector("#list").removeChild();
+// }
